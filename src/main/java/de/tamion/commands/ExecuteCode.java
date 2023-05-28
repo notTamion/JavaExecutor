f8822adea6cf2f65132eaeb06cdbee18ddb66e58
Tamion <simon.dorner.obf@gmail.com>
2023-05-28 13:09:08 +0200

    Changed /execute to /executecode to prevent confusion with the default /execute command

--- src/main/java/de/tamion/commands/ExecuteCode.java
+++ src/main/java/de/tamion/commands/ExecuteCode.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-public class Execute implements CommandExecutor {
+public class ExecuteCode implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
